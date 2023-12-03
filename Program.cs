01340185d5c8a3b138984ec542f4b897a366518c
Marijn Besseling <njirambem@gmail.com>
2023-12-03 09:45:15 +0100

    d3p1

--- Program.cs
+++ Program.cs
@@ -2,53 +2,80 @@
 using System.Data.Common;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text.RegularExpressions;
 
-Regex gameID = new Regex(@"^Game (\d+):");
-Regex redAmt = new Regex(@"(\d+) red");
-Regex greenAmt = new Regex(@"(\d+) green");
-Regex blueAmt = new Regex(@"(\d+) blue");
-
-
-(int id, int red, int green, int blue) toGame(string line)
+public partial class Program
 {
-    var gameIDMatch = gameID.Match(line);
-    int redMax = getMax(line, redAmt);
-    int greenMax = getMax(line, greenAmt);
-    int blueMax = getMax(line, blueAmt);
+    private record struct Poi(int Xh, int Xt, int Y, int? Val);
 
-    return (
-        id: int.Parse(gameIDMatch.Groups[1].ValueSpan),
-        red: redMax,
-        green: greenMax,
-        blue: blueMax
-    );
-
-    static int getMax(string line, Regex regex)
+    private static List<Poi> GetPoi(string line, int y)
     {
-        return regex.Matches(line).Select(m => int.Parse(m.Groups[1].ValueSpan)).Max();
+        List<Poi> pois = new();
+
+        foreach (var match in PoiRegex().EnumerateMatches(line))
+        {
+            pois.Add(new Poi(
+                Xh: match.Index,
+                Xt: match.Index + match.Length - 1,
+                Y: y,
+                Val: int.TryParse(line.AsSpan(match.Index, match.Length), out int val) ? val : null
+            ));
+        }
+
+        return pois;
     }
-}
 
-int power((int id, int red, int green, int blue) game) {
-    return game.red * game.green * game.blue;
-}
-
-// Run("testp1", 142);
-Run("test", 2286);
-Run("input");
-
-void Run(string type, int? expected = null)
-{
-    string[] input = File.ReadAllLines($"{type}-d2.txt");
-
-    var output = input.Select(toGame).Select(power).Sum();
-
-    Console.Write($"{type}:\t{output}");
-
-    if (expected.HasValue) {
-        Console.Write($"\texpected:\t{expected}");
-        Debug.Assert(expected == output);
+    public static void Main()
+    {
+        // Run("testp1", 142);
+        Run("test", 4361);
+        Run("input");
     }
-    Console.WriteLine();
+
+    private static void Run(string type, int? expected = null)
+    {
+        string[] input = File.ReadAllLines($"{type}-d3.txt");
+
+        var pois = input
+            .SelectMany(GetPoi)
+            .ToLookup(p => p.Y);
+
+        int output = 0;
+        foreach (var row in pois)
+        {
+            foreach (var symbol in row.Where(p => !p.Val.HasValue))
+            {
+                var parts = GetParts(pois, symbol, 0)
+                            .Concat(GetParts(pois, symbol, -1))
+                            .Concat(GetParts(pois, symbol, 1))
+                            .Select(p => p.Val!.Value)
+                            .Distinct();
+
+                output += parts.Sum();
+
+                Console.WriteLine(String.Join(", ", parts));
+            }
+        }
+
+        Console.Write($"{type}:\t{output}");
+
+        if (expected.HasValue)
+        {
+            Console.Write($"\texpected:\t{expected}");
+            Debug.Assert(expected == output);
+        }
+        Console.WriteLine();
+    }
+
+    private static IEnumerable<Poi> GetParts(ILookup<int, Poi> output, Poi symbol, int lineOffset)
+    {
+        return output[symbol.Y + lineOffset]
+            .Where(p => p.Val.HasValue 
+                && symbol.Xh <= p.Xt + 1
+                && symbol.Xt >= p.Xh - 1);
+    }
+
+    [GeneratedRegex(@"\d+|[^.\n]")]
+    private static partial Regex PoiRegex();
 }
