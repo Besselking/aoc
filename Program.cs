3c5032e8305affcf8a53115acfdfb873098e0551
Marijn Besseling <njirambem@gmail.com>
2024-10-30 12:50:12 +0100

    d5p1

--- Program.cs
+++ Program.cs
@@ -9,14 +9,14 @@ public static partial class Program
     public static void Main()
     {
         // Run("testp1", 142);
-        Run("test", 30);
+        Run("test", 35);
         Run("input");
     }
 
     private static void Run(string type, int? expected = null)
     {
-        string[] input = File.ReadAllLines($"{type}-d4.txt");
-        int output = GetOutput(input);
+        string[] input = File.ReadAllLines($"{type}-d5.txt");
+        var output = GetOutput(input);
 
         Console.Write($"{type}:\t{output}");
 
@@ -31,105 +31,188 @@ public static partial class Program
 
     // Implementation
 
-    record Metrics(
-        Range IdRange,
-        Range WinnerRange,
-        Range NumberRange);
-
-    record Card(int Id, SortedSet<int> Winners, int[] Numbers)
+    private static long GetOutput(ReadOnlySpan<string> input)
     {
-        public readonly Lazy<int> Score = new Lazy<int>(() => CountScore(Winners, Numbers));
-    }
-
-    static Card ParseCard(Metrics metrics, ReadOnlySpan<char> line)
-    {
-        // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-        int id = int.Parse(line[metrics.IdRange]);
-        List<int> winners = [];
-        ReadOnlySpan<char> winnerSpan = line[metrics.WinnerRange];
-        foreach (Range winner in winnerSpan.Split(' '))
+        // seeds: 79 14 55 13
+        List<long> seeds = [];
+        var seedsLine = input[0].AsSpan("seeds: ".Length);
+        foreach (Range seedRange in seedsLine.Split(' '))
         {
-            ReadOnlySpan<char> s = winnerSpan[winner];
-            if (s.IsEmpty) continue;
-            winners.Add(int.Parse(s));
+            seeds.Add(long.Parse(seedsLine[seedRange]));
         }
 
-        List<int> numbers = [];
-        ReadOnlySpan<char> numberSpan = line[metrics.NumberRange];
-        foreach (Range number in numberSpan.Split(' '))
+        input = input[2..];
+        Debug.Assert(input[0] == "seed-to-soil map:");
+        input = input[1..];
+
+        List<Mapping> seedToSoilMap = [];
+
+        int i;
+        for (i = 0; i < input.Length && input[i] != ""; i++)
         {
-            ReadOnlySpan<char> s = numberSpan[number];
-            if (s.IsEmpty) continue;
-            numbers.Add(int.Parse(s));
+            var mapLine = input[i].AsSpan();
+            Mapping item = GetMapping(mapLine, i);
+
+            seedToSoilMap.Add(item);
         }
 
-        return new Card(id, [.. winners], [.. numbers]);
-    }
+        input = input[(i + 1)..];
+        Debug.Assert(input[0] == "soil-to-fertilizer map:");
+        input = input[1..];
 
-    private static int GetOutput(string[] input)
-    {
-        Metrics metrics = GetMetrics(input[0]);
+        List<Mapping> soilToFertilizerMap = [];
 
-        var cardset = input.Select(line => ParseCard(metrics, line)).ToFrozenDictionary(card => card.Id);
-
-        int cardCount = 0;
-        Queue<Card> cardQueue = new Queue<Card>(cardset.Values);
-
-        while (cardQueue.Count > 0)
+        for (i = 0; i < input.Length && input[i] != ""; i++)
         {
-            Card card = cardQueue.Dequeue();
-            cardCount++;
-            int score = card.Score.Value;
-            if (score == 0) continue;
+            var mapLine = input[i].AsSpan();
+            Mapping item = GetMapping(mapLine, i);
 
-            for (int i = card.Id + 1; i < score + card.Id + 1; i++)
-            {
-                if (cardset.TryGetValue(i, out Card? wonCard))
-                {
-                    cardQueue.Enqueue(wonCard);
-                }
-            }
+            soilToFertilizerMap.Add(item);
         }
 
-        return cardCount;
-    }
+        input = input[(i + 1)..];
+        Debug.Assert(input[0] == "fertilizer-to-water map:");
+        input = input[1..];
 
-    private static int CountScore(SortedSet<int> winners, int[] numbers)
-    {
-        int count = 0;
+        List<Mapping> fertilizerToWaterMap = [];
 
-        foreach (var i in numbers)
+        for (i = 0; i < input.Length && input[i] != ""; i++)
         {
-            if (winners.Contains(i)) count++;
+            var mapLine = input[i].AsSpan();
+            Mapping item = GetMapping(mapLine, i);
+
+            fertilizerToWaterMap.Add(item);
         }
 
-        return count;
-    }
+        input = input[(i + 1)..];
+        Debug.Assert(input[0] == "water-to-light map:");
+        input = input[1..];
 
-    private static Metrics GetMetrics(string line)
-    {
-        Match match = MetricRegex().Match(line);
+        List<Mapping> waterToLightMap = [];
 
-        Range idRange = match.Groups[1].Index..(match.Groups[1].Index + match.Groups[1].Length);
-        Range winnersRange = match.Groups[2].Index..(match.Groups[2].Index + match.Groups[2].Length);
-        Range numbersRange = match.Groups[3].Index..(match.Groups[3].Index + match.Groups[3].Length);
-
-        return new Metrics(idRange, winnersRange, numbersRange);
-    }
-
-    // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-    [GeneratedRegex(@"^Card(\s+\d+): ((?:\s*\d{1,2})+) \| ((?:\s*\d{1,2})+)$")]
-    private static partial Regex MetricRegex();
-
-    private static IEnumerable<TValue> PickFrom<TKey, TValue>(this IEnumerable<TKey> source,
-        IDictionary<TKey, TValue> dict)
-    {
-        foreach (TKey key in source)
+        for (i = 0; i < input.Length && input[i] != ""; i++)
         {
-            if (dict.TryGetValue(key, out TValue? value) && value != null)
-            {
-                yield return value;
-            }
+            var mapLine = input[i].AsSpan();
+            Mapping item = GetMapping(mapLine, i);
+
+            waterToLightMap.Add(item);
+        }
+
+        input = input[(i + 1)..];
+        Debug.Assert(input[0] == "light-to-temperature map:");
+        input = input[1..];
+
+        List<Mapping> lightToTempMap = [];
+
+        for (i = 0; i < input.Length && input[i] != ""; i++)
+        {
+            var mapLine = input[i].AsSpan();
+            Mapping item = GetMapping(mapLine, i);
+
+            lightToTempMap.Add(item);
+        }
+
+        input = input[(i + 1)..];
+        Debug.Assert(input[0] == "temperature-to-humidity map:");
+        input = input[1..];
+
+        List<Mapping> tempToHumidMap = [];
+
+        for (i = 0; i < input.Length && input[i] != ""; i++)
+        {
+            var mapLine = input[i].AsSpan();
+            Mapping item = GetMapping(mapLine, i);
+
+            tempToHumidMap.Add(item);
+        }
+
+        input = input[(i + 1)..];
+        Debug.Assert(input[0] == "humidity-to-location map:");
+        input = input[1..];
+
+        List<Mapping> humidToLocMap = [];
+
+        for (i = 0; i < input.Length && input[i] != ""; i++)
+        {
+            var mapLine = input[i].AsSpan();
+            Mapping item = GetMapping(mapLine, i);
+
+            humidToLocMap.Add(item);
+        }
+
+        seedToSoilMap.Sort();
+        soilToFertilizerMap.Sort();
+        fertilizerToWaterMap.Sort();
+        waterToLightMap.Sort();
+        lightToTempMap.Sort();
+        tempToHumidMap.Sort();
+        humidToLocMap.Sort();
+
+        long minLoc = long.MaxValue;
+        foreach (long seed in seeds)
+        {
+            long soil = GetDest(seedToSoilMap, seed);
+            long fert = GetDest(soilToFertilizerMap, soil);
+            long water = GetDest(fertilizerToWaterMap, fert);
+            long light = GetDest(waterToLightMap, water);
+            long temp = GetDest(lightToTempMap, light);
+            long humid = GetDest(tempToHumidMap, temp);
+            long loc = GetDest(humidToLocMap, humid);
+
+            Console.WriteLine($"Seed {seed}, soil {soil}, fertilizer {fert}, water {water}, light {light}, temperature {temp}, humidity {humid}, location {loc}.");
+
+            minLoc = long.Min(minLoc, loc);
+        }
+
+        return minLoc;
+    }
+
+    private static long GetDest(List<Mapping> mapping, long src)
+    {
+        long dest = src;
+        Mapping? map = mapping.FirstOrDefault(
+            map => map.SrcStart <= src
+                && map.SrcStart + map.RangeLength > src);
+
+        if (map is not null)
+        {
+            dest = map.DestOffset + (src - map.SrcStart);
+        }
+        return dest;
+    }
+
+    private static Mapping GetMapping(ReadOnlySpan<char> mapLine, int i)
+    {
+        int nextSplit = mapLine.IndexOf(' ');
+
+        long destStart = long.Parse(mapLine[..nextSplit]);
+        mapLine = mapLine[(nextSplit + 1)..];
+
+        nextSplit = mapLine.IndexOf(' ');
+        long srcStart = long.Parse(mapLine[..nextSplit]);
+
+        long rangeLength = long.Parse(mapLine[(nextSplit + 1)..]);
+
+        Mapping item = new Mapping(srcStart, rangeLength, destStart);
+        return item;
+    }
+
+    private record Mapping(long SrcStart, long RangeLength, long DestOffset) 
+        : IComparable<Mapping>
+        , IComparable<long>
+    {
+        public int CompareTo(Mapping? other)
+        {
+            if (other is null) return -1;
+
+            return Comparer<long>.Default.Compare(SrcStart, other.SrcStart);
+        }
+
+        public int CompareTo(long other)
+        {
+            if (other < SrcStart) return -1;
+            if (other > SrcStart + RangeLength - 1) return 1;
+            return 0;
         }
     }
 }
