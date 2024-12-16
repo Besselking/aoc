a31c8a29364c8f107e36dfc09caf25378b4f91ed
Marijn Besseling <njirambem@gmail.com>
2024-12-16 21:52:25 +0100

    2024 d16p2

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ public static partial class Program
 {
     public static void Main()
     {
-        Run("test", 7036);
-        Run("test2", 11048);
+        Run("test", 45);
+        Run("test2", 64);
         Run("input");
     }
 
@@ -35,84 +35,103 @@ public static partial class Program
 
         Grid grid = new Grid(input);
 
-        var walkableTiles = grid.IndexesOf('.');
         var start = grid.IndexOf('S');
         var endTile = grid.IndexOf('E');
         Grid.NeighborType startDir = Grid.NeighborType.East;
 
-        PriorityQueue<((int row, int column) pos, Grid.NeighborType direction), int> frontier = new();
-        frontier.Enqueue((start, startDir), 0);
-        HashSet<(int row, int column)> expanded = [];
+        PriorityQueue<((int row, int column) pos, Grid.NeighborType direction, List<(int row, int col)> path), int>
+            frontier = new();
+        frontier.Enqueue((start, startDir, [start]), 0);
+        Dictionary<((int row, int column) pos, Grid.NeighborType direction), int> minScores = new();
+        long bestScore = long.MaxValue;
+        List<List<(int row, int column)>> bestPaths = new();
 
         while (frontier.Count > 0)
         {
             frontier.TryDequeue(out var node, out var cost);
+            if (cost > bestScore)
+            {
+                continue;
+            }
+
             if (node.pos == endTile)
             {
-                sum = cost;
-                break;
+                if (cost == bestScore)
+                {
+                    bestPaths.Add(node.path);
+                }
+                else
+                {
+                    bestPaths = [node.path];
+                    bestScore = cost;
+                }
+
+                continue;
             }
 
-            expanded.Add(node.pos);
-
-            // forward
-            var forward = grid.GetNeighborPos(node.pos, node.direction);
-            if (grid[forward] is '#')
             {
-                //skip
-            }
-            else if (!expanded.Contains(forward)
-                     && frontier.UnorderedItems.All(n => n.Element.pos != forward))
-            {
-                frontier.Enqueue((forward, node.direction), cost + 1);
-            }
-            else if (frontier.UnorderedItems.FirstOrDefault(n => n.Element.pos == forward)
-                         is var forwardInFrontier && forwardInFrontier.Priority > cost + 1)
-            {
-                frontier.Remove(forwardInFrontier.Element, out _, out _);
-                frontier.Enqueue((forward, node.direction), cost + 1);
-            }
-
-            // left turn
-            Grid.NeighborType leftDir = node.direction.TurnLeft().TurnLeft();
-            var left = grid.GetNeighborPos(node.pos, leftDir);
-            var leftCost = cost + 1001;
-            if (grid[left] is '#')
-            {
-                //skip
-            }
-            else if (!expanded.Contains(left)
-                     && frontier.UnorderedItems.All(n => n.Element.pos != left))
-            {
-                frontier.Enqueue((left, leftDir), leftCost);
-            }
-            else if (frontier.UnorderedItems.FirstOrDefault(n => n.Element.pos == left)
-                         is var leftInFrontier && leftInFrontier.Priority > leftCost)
-            {
-                frontier.Remove(leftInFrontier.Element, out _, out _);
-                frontier.Enqueue((left, leftDir), leftCost);
+                // forward
+                var forward = grid.GetNeighborPos(node.pos, node.direction);
+                if (grid[forward] is '#')
+                {
+                    //skip
+                }
+                else
+                {
+                    var forwardCost = cost + 1;
+                    if (!minScores.TryGetValue((forward, node.direction), out var score)
+                        || score >= forwardCost)
+                    {
+                        minScores[(forward, node.direction)] = forwardCost;
+                        frontier.Enqueue((forward, node.direction, [..node.path, forward]), forwardCost);
+                    }
+                }
             }
 
-            // right turn
-            Grid.NeighborType rightDir = node.direction.TurnRight().TurnRight();
-            var right = grid.GetNeighborPos(node.pos, rightDir);
-            var rightCost = cost + 1001;
-            if (grid[right] is '#')
             {
-                //skip
+                // left turn
+                Grid.NeighborType leftDir = node.direction.TurnLeft().TurnLeft();
+                var left = grid.GetNeighborPos(node.pos, leftDir);
+                var leftCost = cost + 1001;
+                if (grid[left] is '#')
+                {
+                    //skip
+                }
+                else if (!minScores.TryGetValue((left, leftDir), out var score)
+                         || score >= leftCost
+                        )
+                {
+                    minScores[(left, leftDir)] = leftCost;
+                    frontier.Enqueue((left, leftDir, [..node.path, left]), leftCost);
+                }
             }
-            else if (!expanded.Contains(right)
-                     && frontier.UnorderedItems.All(n => n.Element.pos != right))
             {
-                frontier.Enqueue((right, rightDir), rightCost);
-            }
-            else if (frontier.UnorderedItems.FirstOrDefault(n => n.Element.pos == right)
-                         is var rightInFrontier && rightInFrontier.Priority > rightCost)
-            {
-                frontier.Remove(rightInFrontier.Element, out _, out _);
-                frontier.Enqueue((right, rightDir), rightCost);
+                // right turn
+                Grid.NeighborType rightDir = node.direction.TurnRight().TurnRight();
+                var right = grid.GetNeighborPos(node.pos, rightDir);
+                var rightCost = cost + 1001;
+                if (grid[right] is '#')
+                {
+                    //skip
+                }
+                else if (!minScores.TryGetValue((right, rightDir), out var score)
+                         || score >= rightCost)
+                {
+                    minScores[(right, rightDir)] = rightCost;
+                    frontier.Enqueue((right, rightDir, [..node.path, right]), rightCost);
+                }
             }
         }
+
+        sum = 0;
+
+        foreach (var path in bestPaths.SelectMany(n => n).Distinct())
+        {
+            grid[path] = 'O';
+            sum++;
+        }
+
+        Console.WriteLine(grid.ToString());
 
         return sum;
     }
