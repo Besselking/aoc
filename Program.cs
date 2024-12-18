633388fb354909888a8048536a2deec80f77c675
Marijn Besseling <njirambem@gmail.com>
2024-12-18 18:24:46 +0100

    2024 d18p1

--- Program.cs
+++ Program.cs
@@ -6,14 +6,18 @@ public static partial class Program
 {
     public static void Main()
     {
-        Run("test", 117440);
+        Bounds = new(7, 7);
+        AfterNBytes = 12;
+        Run("test", 22);
         // Run("test2", 64);
+        Bounds = new(71, 71);
+        AfterNBytes = 1024;
         Run("input");
     }
 
     private static void Run(string type, long? expected = null)
     {
-        var input = File.ReadAllLines($"inputs/{type}-d17.txt");
+        var input = File.ReadAllLines($"inputs/{type}-d18.txt");
         var output = GetOutput(input);
 
         Console.Write($"{type}:\t{output}");
@@ -29,125 +33,100 @@ public static partial class Program
 
     // Implementation
 
+    private static Vec2 Bounds = new Vec2(0, 0);
+    private static int AfterNBytes = 0;
+
     private static long GetOutput(string[] input)
     {
-        long sum = 1;
-        ulong ra = 0; //input[0].AsSpan("Register A: ".Length).ParseAsUint();
-        ulong rb = 0; //input[1].AsSpan("Register B: ".Length).ParseAsUint();
-        ulong rc = 0; //input[2].AsSpan("Register C: ".Length).ParseAsUint();
+        long sum = 0;
 
-        var programSpan = input[4].AsSpan("Program: ".Length);
-        Span<byte> instructions = programSpan.Split(',').ParseAsBytes(programSpan);
+        int[][] bytes = input.Select(s => s.Split(',').Select(int.Parse).ToArray()).ToArray();
 
-        byte[] output = new byte[instructions.Length];
-        int outputCount = 0;
-        ulong initRa;
-        for (initRa = 1; initRa < (ulong)Math.Pow(8, instructions.Length); initRa++)
+
+        char[] gridData = new char[(Bounds.X) * (Bounds.Y)];
+        Array.Fill(gridData, '.');
+        Grid grid = new(gridData, Bounds.X, Bounds.Y);
+
+        foreach (var b in bytes.Take(AfterNBytes))
         {
-            outputCount = 0;
-            ra = initRa;
-            rb = 0;
-            rc = 0;
+            grid[b[1], b[0]] = '#';
+        }
 
-            for (int i = 0; i + 1 < instructions.Length;)
+        var endPos = (Bounds.X - 1, Bounds.Y - 1);
+        PriorityQueue<((int row, int column) pos, Grid.NeighborType direction), int> frontier = new();
+        frontier.Enqueue(((0, 0), Grid.NeighborType.East), 0);
+        HashSet<(int row, int column)> expanded = [];
+
+        while (frontier.Count > 0)
+        {
+            frontier.TryDequeue(out var node, out var cost);
+            if (node.pos == endPos)
             {
-                byte opcode = instructions[i];
-                if (output.Length > instructions.Length) break;
-                byte operand = instructions[i + 1];
-
-                switch (opcode)
-                {
-                    case 0: // adv, A division
-                    {
-                        var numerator = ra;
-                        var combo = GetComboOperand(operand);
-                        double denominator = Math.Pow(2, combo);
-                        ra = (ulong)(numerator / denominator);
-                        break;
-                    }
-                    case 1: // bxl, B xor
-                    {
-                        rb ^= operand;
-                        break;
-                    }
-                    case 2: // bst, B modulo
-                    {
-                        var combo = GetComboOperand(operand);
-                        rb = (ulong)combo % 8;
-                        break;
-                    }
-                    case 3: // jnz, jump not zero
-                    {
-                        if (ra == 0) break;
-                        i = (int)operand;
-                        continue;
-                    }
-                    case 4: // bxc, B xor C
-                    {
-                        rb ^= rc;
-                        break;
-                    }
-                    case 5: //out
-                    {
-                        var combo = GetComboOperand(operand);
-                        var outval = combo % 8;
-                        bool overflow = Output((byte)outval);
-                        if (overflow) goto reset;
-                        break;
-                    }
-                    case 6: // bdv, B division
-                    {
-                        var numerator = ra;
-                        var combo = GetComboOperand(operand);
-                        var denominator = Math.Pow(2, combo);
-                        rb = (ulong)(numerator / denominator);
-                        break;
-                    }
-                    case 7: // cdv, C division
-                    {
-                        var numerator = ra;
-                        var combo = GetComboOperand(operand);
-                        double denominator = Math.Pow(2, combo);
-                        rc = (ulong)(numerator / denominator);
-                        break;
-                    }
-                }
-
-                i += 2;
+                sum = cost;
+                break;
             }
 
-            reset:
-            if (outputCount > 0 && instructions.EndsWith(output.AsSpan(0, outputCount)))
+            expanded.Add(node.pos);
+
+            // forward
+            var forward = grid.GetNeighborPos(node.pos, node.direction);
+            if (!grid.InBounds(forward) || grid[forward] is '#')
             {
-                if (outputCount == instructions.Length) break;
-                initRa = (initRa * 8) - 1;
+                //skip
+            }
+            else if (!expanded.Contains(forward)
+                     && frontier.UnorderedItems.All(n => n.Element.pos != forward))
+            {
+                frontier.Enqueue((forward, node.direction), cost + 1);
+            }
+            else if (frontier.UnorderedItems.FirstOrDefault(n => n.Element.pos == forward)
+                         is var forwardInFrontier && forwardInFrontier.Priority > cost + 1)
+            {
+                frontier.Remove(forwardInFrontier.Element, out _, out _);
+                frontier.Enqueue((forward, node.direction), cost + 1);
+            }
+
+            // left turn
+            Grid.NeighborType leftDir = node.direction.TurnLeft().TurnLeft();
+            var left = grid.GetNeighborPos(node.pos, leftDir);
+            var leftCost = cost + 1;
+            if (!grid.InBounds(left) || grid[left] is '#')
+            {
+                //skip
+            }
+            else if (!expanded.Contains(left)
+                     && frontier.UnorderedItems.All(n => n.Element.pos != left))
+            {
+                frontier.Enqueue((left, leftDir), leftCost);
+            }
+            else if (frontier.UnorderedItems.FirstOrDefault(n => n.Element.pos == left)
+                         is var leftInFrontier && leftInFrontier.Priority > leftCost)
+            {
+                frontier.Remove(leftInFrontier.Element, out _, out _);
+                frontier.Enqueue((left, leftDir), leftCost);
+            }
+
+            // right turn
+            Grid.NeighborType rightDir = node.direction.TurnRight().TurnRight();
+            var right = grid.GetNeighborPos(node.pos, rightDir);
+            var rightCost = cost + 1;
+            if (!grid.InBounds(right) || grid[right] is '#')
+            {
+                //skip
+            }
+            else if (!expanded.Contains(right)
+                     && frontier.UnorderedItems.All(n => n.Element.pos != right))
+            {
+                frontier.Enqueue((right, rightDir), rightCost);
+            }
+            else if (frontier.UnorderedItems.FirstOrDefault(n => n.Element.pos == right)
+                         is var rightInFrontier && rightInFrontier.Priority > rightCost)
+            {
+                frontier.Remove(rightInFrontier.Element, out _, out _);
+                frontier.Enqueue((right, rightDir), rightCost);
             }
         }
 
-        // sum = String.Join("", output.Select(x => x.ToString())).AsSpan().ParseAsLong();
-        Console.WriteLine(initRa);
-        return (long)initRa;
-
-        ulong GetComboOperand(byte combo)
-        {
-            return combo switch
-            {
-                <= 3 => combo,
-                4 => ra,
-                5 => rb,
-                6 => rc,
-                7 => throw new InvalidOperationException("invalid program 7 is reserved"),
-                _ => throw new InvalidOperationException($"invalid program, not an operator '{combo}'"),
-            };
-        }
-
-        bool Output(byte value)
-        {
-            // if (output.Count != 0) Console.Write(',');
-            // Console.Write(value);
-            if (outputCount == output.Length) return true;
-            output[outputCount++] = value;
-            return false;
-        }
+        return sum;
     }
 }
