a7ef66930062d1c0d54299b1b70b0c0474803b55
Marijn Besseling <njirambem@gmail.com>
2024-12-19 21:07:32 +0100

    2024 d19p2

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Text.RegularExpressions;
 
 namespace aoc;
 
@@ -7,8 +6,8 @@ public static partial class Program
 {
     public static void Main()
     {
-        Run("test", 6);
-        Run("input");
+        Run("test", 16);
+        Run("input", 584553405070389);
     }
 
     private static void Run(string type, long? expected = null)
@@ -29,17 +28,42 @@ public static partial class Program
 
     // Implementation
 
+    private static string[] towels;
+    private static Dictionary<string, long> cache = new Dictionary<string, long>();
+
+    private static Dictionary<string, long>.AlternateLookup<ReadOnlySpan<char>> cacheLookup =
+        cache.GetAlternateLookup<ReadOnlySpan<char>>();
+
     private static long GetOutput(string[] input)
     {
         long sum = 0;
 
-        string[] towels = input[0].Split(", ");
+        towels = input[0].Split(", ");
         string[] patterns = input[2..];
 
-        Regex towelRx = new Regex($"^({String.Join("|", towels)})+$", RegexOptions.Compiled);
-
-        sum = patterns.Count(s => towelRx.IsMatch(s));
+        cache.Clear();
+        sum = 0;
+        foreach (var s in patterns)
+        {
+            sum += GetMatchCount(s);
+        }
 
         return sum;
+    }
+
+    private static long GetMatchCount(ReadOnlySpan<char> pattern)
+    {
+        if (pattern.IsEmpty) return 1;
+        if (cacheLookup.TryGetValue(pattern, out long count)) return count;
+
+        foreach (var towel in towels)
+        {
+            if (pattern.StartsWith(towel))
+            {
+                count += GetMatchCount(pattern[towel.Length..]);
+            }
+        }
+
+        return cacheLookup[pattern] = count;
     }
 }
