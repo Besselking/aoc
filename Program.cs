a95cef78b8f2e024c6788ec6797b500e7e4a2e86
Marijn Besseling <njirambem@gmail.com>
2024-12-20 20:47:43 +0100

    2024 d20p1

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ public static partial class Program
 {
     public static void Main()
     {
-        Run("test", 16);
-        Run("input", 584553405070389);
+        Run("test", 0);
+        Run("input");
     }
 
     private static void Run(string type, long? expected = null)
     {
-        var input = File.ReadAllLines($"inputs/{type}-d19.txt");
+        var input = File.ReadAllLines($"inputs/{type}-d20.txt");
         var output = GetOutput(input);
 
         Console.Write($"{type}:\t{output}");
@@ -28,42 +28,68 @@ public static partial class Program
 
     // Implementation
 
-    private static string[] towels;
-    private static Dictionary<string, long> cache = new Dictionary<string, long>();
-
-    private static Dictionary<string, long>.AlternateLookup<ReadOnlySpan<char>> cacheLookup =
-        cache.GetAlternateLookup<ReadOnlySpan<char>>();
-
     private static long GetOutput(string[] input)
     {
         long sum = 0;
 
-        towels = input[0].Split(", ");
-        string[] patterns = input[2..];
+        Grid grid = new Grid(input);
 
-        cache.Clear();
-        sum = 0;
-        foreach (var s in patterns)
+        int threshhold = 100;
+
+        var nPathtiles = grid.Count('.');
+        var pathTiles = new (int row, int col)[nPathtiles + 2];
+        var startTile = grid.IndexOf('S');
+        var endTile = grid.IndexOf('E');
+
+        pathTiles[0] = startTile;
+        pathTiles[^1] = endTile;
+
+        var prevTile = startTile;
+        var currentTile = startTile;
+        Span<(int row, int col)> neighbors = stackalloc (int row, int col)[2];
+        for (int i = 1; i < pathTiles.Length - 1; i++)
         {
-            sum += GetMatchCount(s);
-        }
+            int nbCount = grid.NeighborsOf(neighbors, currentTile, '.', Grid.NeighborType.Orthogonal);
 
-        return sum;
-    }
-
-    private static long GetMatchCount(ReadOnlySpan<char> pattern)
-    {
-        if (pattern.IsEmpty) return 1;
-        if (cacheLookup.TryGetValue(pattern, out long count)) return count;
-
-        foreach (var towel in towels)
-        {
-            if (pattern.StartsWith(towel))
+            foreach (var nb in neighbors[..nbCount])
             {
-                count += GetMatchCount(pattern[towel.Length..]);
+                if (nb == prevTile)
+                {
+                    continue;
+                }
+
+                pathTiles[i] = nb;
+                prevTile = currentTile;
+                currentTile = nb;
+                break;
             }
         }
 
-        return cacheLookup[pattern] = count;
+        for (int i = 0; i < pathTiles.Length; i++)
+        {
+            for (int j = pathTiles.Length - 1; j > i; j--)
+            {
+                var left = pathTiles[i];
+                var right = pathTiles[j];
+
+                if (Math.Abs(left.row - right.row)
+                    + Math.Abs(left.col - right.col) == 2)
+                {
+                    var middle = ((left.row + right.row) / 2, (left.col + right.col) / 2);
+                    if (grid[middle] is '#')
+                    {
+                        var diff = Math.Abs(i - j) - 2;
+
+                        if (diff >= threshhold)
+                        {
+                            sum++;
+                        }
+                        // Console.WriteLine($"left {i} {left}, right {j} {right}, middle {middle}, diff {diff}");
+                    }
+                }
+            }
+        }
+
+        return sum;
     }
 }
