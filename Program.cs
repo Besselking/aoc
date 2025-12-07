2b58252fcc66118d6e0604a3746061b198a4a775
Marijn Besseling <njirambem@gmail.com>
2025-12-07 21:17:40 +0100

    day07p1

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ public static partial class Program
 {
     public static void Main()
     {
-        Run("test", 3);
+        Run("test", 21);
         // Run("test", 2024);
         Run("input");
     }
 
     private static void Run(string type, long? expected = null)
     {
-        var input = File.ReadAllLines($"Inputs/2025/{type}-01.txt");
+        var input = File.ReadAllLines($"Inputs/2025/{type}-07.txt");
         var output = GetOutput(input);
 
         Console.Write($"{type}:\t{output}");
@@ -32,21 +32,34 @@ public static partial class Program
     private static long GetOutput(ReadOnlySpan<string> input)
     {
         long sum = 0;
-        int dial = 50;
 
-        foreach (var line in input)
+        Grid grid = new(input.ToArray());
+
+        for (var row = 0; row < grid.Rows - 1; row++)
         {
-            if (dial == 0) sum++;
-            char dir = line[0];
-            int rot = line.AsSpan()[1..].ParseAsInt();
-
-            dial += dir switch
+            for (var col = 0; col < grid.Cols; col++)
             {
-                'L' => -rot,
-                'R' => rot,
-            };
+                var cell = grid[row, col];
+                if (cell == '.') continue;
+                if (cell == 'S')
+                {
+                    grid[row + 1, col] = '|';
+                }
+                else if (cell == '^' && grid[row - 1, col] == '|')
+                {
+                    sum++;
+                    grid[row, col - 1] = '|';
+                    grid[row, col + 1] = '|';
+                    grid[row + 1, col - 1] = '|';
+                    grid[row + 1, col + 1] = '|';
+                }
+                else if (cell == '|' && grid[row + 1, col] == '.')
+                {
+                    grid[row + 1, col] = '|';
+                }
+            }
 
-            dial %= 100;
+            Console.WriteLine(grid.ToString());
         }
 
         return sum;
